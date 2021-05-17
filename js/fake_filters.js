373c43baa49f8c1672428b9398e337a3cd4bf6c3
human97 <aleksey.n.popov@gmail.com>
2021-05-17 16:14:44 +0400

    fix js, fonts

--- js/fake_filters.js
+++ js/fake_filters.js
@@ -1,78 +1,46 @@
-const fakeFilters = [{
-    name: 3,
-    checked: false
-  },
-  {
-    name: 4,
-    checked: false
-  },
-  {
-    name: 5,
-    checked: false
-  },
-  {
-    name: 6,
-    checked: false
-  },
-  {
-    name: 7,
-    checked: false
-  },
-  {
-    name: 8,
-    checked: false
-  },
-  {
-    name: 9,
-    checked: false
-  },
-  {
-    name: 10,
-    checked: false
-  },
-]
+window.addEventListener('load', () => {
 
-const filters = document.querySelector('.filters');
-filters.innerHTML = `
-<h3 class="filters_title">Диаметр (мм)</h3>
-<div class="filters_wrapper">
-</div>
-<!-- /.filters_wrapper -->
-<button class='filters--show_more'>Показать еще</button>`
-
-
-const filtersWrapper = document.querySelector('.filters_wrapper');
-
-const showCount = 4;
-const showMore = document.querySelector('.filters--show_more');
-
-const list = fakeFilters.map((el, index) => {
-  const div = document.createElement('div');
-
-  const template = `<div class="filter_wrapper ${
-    index < showCount ? '' : 'hidden'
-  }">
-  <label class="filter_checkbox_label" for="filter_checkbox">
-    <input class="filter_checkbox" type="checkbox" id="filter_checkbox" chcked=${el.checked}>
-    ${el.name}
-  </label>
-</div>
-<!-- /.filter_wrapper -->`;
-  div.innerHTML = template;
-  return div;
-});
-
-list.forEach(el => {
-  filtersWrapper.appendChild(el);
-});
-
-console.log(showMore)
-
-showMore.addEventListener('click', () => {
+  const filters = document.querySelector('.filters');
+  const hideAll = document.querySelector('.filters--hide_all_filters_btn');
   const items = document.querySelectorAll('.filter_wrapper');
+  const filtersWrapper = document.querySelector('.filters_wrapper');
+  const showCount = 4;
+  const showMore = document.querySelector('.filters--show_more');
+
+
+  // показываем до 4-х фильтров включительно
   items.forEach((el, i) => {
-    if (i > showCount) {
-      el.classList.toggle('hidden');
+    if (i < 4) {
+      el.classList.remove('hidden_filters');
     }
   });
+
+
+  // показываем/скрываем остальные фильтры и меняем текст в кнопке(Показать еще/Скрыть)
+  showMore.addEventListener('click', () => {
+
+    //меняем текст в кнопке
+    if (showMore.textContent === 'Показать еще') {
+      showMore.textContent = 'Скрыть'
+    } else {
+      showMore.textContent = 'Показать еще'
+    }
+
+    // показать/скрыть остальные фильтры
+    items.forEach((el, i) => {
+      if (i > showCount) {
+        el.classList.toggle('hidden_filters');
+      }
+    });
+
+  });
+
+  // скрыть/показать блок с фильтрами по клику на filtersTitle
+  hideAll.addEventListener('click', () => {
+    const hideAllArrow = document.querySelector('.hide_all_filters_btn--arrow');
+    hideAllArrow.classList.toggle('btn--arrow_rotate');
+
+    filtersWrapper.classList.toggle('hidden_filters');
+  })
+
 });
